dacb70694bbac641c85d53f59b99d1190cdd57c2
Mesfin <mesbekmek@gmail.com>
2015-08-16 15:44:52 -0400

    First commit

---
+++ ShoutOut/ShoutOut/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ShoutOut
+//
+//  Created by Mesfin Bekele Mekonnen on 8/13/15.
+//  Copyright © 2015 Mesfin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ShoutOut/ShoutOut/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  ShoutOut
+//
+//  Created by Mesfin Bekele Mekonnen on 8/13/15.
+//  Copyright © 2015 Mesfin. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <Parse/Parse.h>
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+}
+
+@end
