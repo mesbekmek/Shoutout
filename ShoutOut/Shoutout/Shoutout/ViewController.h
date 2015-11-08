f7ffff2097974549cbbe2b9c3a2bcd148c965cbd
Mesfin <mesbekmek@gmail.com>
2015-11-08 15:44:57 -0500

    Created new project with CoreData

---
+++ ShoutOut/Shoutout/Shoutout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Shoutout
+//
+//  Created by Mesfin Bekele Mekonnen on 11/8/15.
+//  Copyright © 2015 Mesfin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ShoutOut/Shoutout/Shoutout/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Shoutout
+//
+//  Created by Mesfin Bekele Mekonnen on 11/8/15.
+//  Copyright © 2015 Mesfin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
