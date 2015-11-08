b84f3d3bdefb99df004f298ef48665ac3917af67
Jason Wang <qwang216@gmail.com>
2015-11-08 16:48:18 -0500

    added project and video model

---
+++ ShoutOut/Shoutout/Shoutout/Project.m
@@ -0,0 +1,13 @@
+//
+//  Project.m
+//  Shoutout
+//
+//  Created by Jason Wang on 11/8/15.
+//  Copyright © 2015 Mesfin. All rights reserved.
+//
+
+#import "Project.h"
+
+@implementation Project
+
+@end
---
+++ ShoutOut/Shoutout/Shoutout/Video.m
@@ -0,0 +1,13 @@
+//
+//  Video.m
+//  Shoutout
+//
+//  Created by Jason Wang on 11/8/15.
+//  Copyright © 2015 Mesfin. All rights reserved.
+//
+
+#import "Video.h"
+
+@implementation Video
+
+@end
