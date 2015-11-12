711d619a9e7cfae1843afbd4f32591a78bc74268
Diana <diana_elezaj@hotmail.com>
2015-11-12 16:26:10 -0500

    Added Sorting video VC and files

--- ShoutOut/Shoutout/Shoutout/SOSortingViewController.m
+++ ShoutOut/Shoutout/Shoutout/SOSortingViewController.m
@@ -7,8 +7,17 @@
 //
 
 #import "SOSortingViewController.h"
+#import <MediaPlayer/MediaPlayer.h>
+#import "SOSortingCVC.h"
 
-@interface SOSortingViewController ()
+
+@interface SOSortingViewController ()<UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+{
+    IBOutlet UIView *centerView;
+    IBOutlet UICollectionView *collectionView;
+    
+    NSMutableArray *imagesArray;
+}
 
 @end
 
@@ -16,22 +25,97 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    imagesArray = [[NSMutableArray alloc] initWithObjects:@"video1.jpg", @"video2.jpg", @"video3.jpg", nil];
+    
+    
+    // This nib file has a "live area" defined by an inner view. It's background is necessarily transparent
+    UINib *myNib = [UINib nibWithNibName:@"SOSortingCollectionViewCell" bundle:nil];
+    
+    [collectionView registerNib:myNib forCellWithReuseIdentifier:@"sortingIdentifier"];
+    
+    // By turning off clipping, you'll see the prior and next items.
+    collectionView.clipsToBounds = NO;
+    
+    UICollectionViewFlowLayout *myLayout = [[UICollectionViewFlowLayout alloc] init];
+    
+    CGFloat margin = ((self.view.frame.size.width - collectionView.frame.size.width) / 2);
+    
+    // This assumes that the the collectionView is centered withing its parent view.
+    myLayout.itemSize = CGSizeMake(collectionView.frame.size.width + margin, collectionView.frame.size.height);
+    
+    // A negative margin will shift each item to the left.
+    myLayout.minimumLineSpacing = -margin;
+    
+    myLayout.scrollDirection = UICollectionViewScrollDirectionHorizontal;
+    
+    [collectionView setCollectionViewLayout:myLayout];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+
+
+
+
+
+
+
+#pragma mark - UICollectionViewDataSourceDelegate
+
+
+
+- (NSInteger)collectionView:(UICollectionView *)aCollectionView
+     numberOfItemsInSection:(NSInteger)aSection
+{
+    //    NSUInteger count = 4;
+    //
+    //    return count;
+    return [imagesArray count];
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (UICollectionViewCell *)collectionView:(UICollectionView *)aCollectionView
+                  cellForItemAtIndexPath:(NSIndexPath *)anIndexPath
+{
+    SOSortingCVC *appropriateCell = [aCollectionView dequeueReusableCellWithReuseIdentifier:@"sortingIdentifier"
+                                                                             forIndexPath:anIndexPath];
+    NSString *videoImage = [imagesArray objectAtIndex:anIndexPath.row];
+    
+    NSLog(@"%@",videoImage);
+    
+    
+    UIImage *image = [UIImage imageNamed: videoImage];
+    
+    
+    appropriateCell.videoImageView.image = image;
+    
+    
+    NSLog(@"imagessss %@",[imagesArray objectAtIndex:anIndexPath.row]);
+    return appropriateCell;
 }
-*/
+
+- (UIEdgeInsets)collectionView:(UICollectionView *)aCollectionView
+                        layout:(UICollectionViewFlowLayout *)aCollectionViewLayout
+        insetForSectionAtIndex:(NSInteger)aSection
+{
+    CGFloat margin = (aCollectionViewLayout.minimumLineSpacing / 2);
+    
+    // top, left, bottom, right
+    UIEdgeInsets myInsets = UIEdgeInsetsMake(0, margin, 0, margin);
+    
+    return myInsets;
+}
+
+
+//-(void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath
+//{
+//    
+//    [self performSegueWithIdentifier:@"SortingVideos" sender:self];
+//    
+//    
+//    
+//}
+
+
+
+
 
 @end
