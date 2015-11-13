1853f7b2c2af0d4288f9cfed47ad731d4adf2e32
Jason Wang <qwang216@gmail.com>
2015-11-13 15:48:13 -0500

    still working on contact

---
+++ ShoutOut/Shoutout/Shoutout/SearchContactTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SearchContactTableViewController.h
+//  Shoutout
+//
+//  Created by Jason Wang on 11/13/15.
+//  Copyright © 2015 Mesfin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchContactTableViewController : UITableViewController
+
+@end
