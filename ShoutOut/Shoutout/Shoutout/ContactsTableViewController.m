5bf752fe6b0db6eae64f12cd84f9265f1efd8ec0
Jason Wang <qwang216@gmail.com>
2015-11-15 00:12:20 -0500

    contact tvc

--- ShoutOut/Shoutout/Shoutout/ContactsTableViewController.m
+++ ShoutOut/Shoutout/Shoutout/ContactsTableViewController.m
@@ -20,102 +20,32 @@
 
 @implementation ContactsTableViewController
 
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     
     PFUser *currentUser =  [PFUser currentUser];
-    NSLog(@"current user  contacts == %@",[currentUser objectForKey: @"contacts"] );
+    self.currentUserContacts = [currentUser objectForKey:@"contacts"];
+    NSLog(@"%@",self.currentUserContacts);
     
-    PFQuery *query = [PFUser query];
-
-    [query whereKey:@"username" containedIn: [currentUser objectForKey:@"contacts"]];
-    [query findObjectsInBackgroundWithBlock:^(NSArray * _Nullable objects, NSError * _Nullable error) {
-        NSLog(@"%@",objects);
-        self.storeUsersContacts = objects;
-        [self grabContacts];
-        NSLog(@"%@",error);
-    }];
-    
-}
-
--(void)grabContacts {
-    for (PFUser *contact in self.storeUsersContacts) {
-        if ([[contact objectForKey:@"contacts"] containsObject: [PFUser currentUser][@"username"]]) {
-            [self.friendedContacts addObject:contact];
-        }
-        NSLog(@"contacts == %@",[contact objectForKey:@"contacts"]);
-    }
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-#warning Incomplete implementation, return the number of sections
-    return 0;
+    return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-#warning Incomplete implementation, return the number of rows
-    return 0;
+    return self.currentUserContacts.count;
 }
 
-/*
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
-    
-    // Configure the cell...
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"currentUserContactsCellID" forIndexPath:indexPath];
+    cell.textLabel.text = self.currentUserContacts[indexPath.row];
     
     return cell;
 }
-*/
-
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
