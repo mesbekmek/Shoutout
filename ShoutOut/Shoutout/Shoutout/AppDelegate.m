51f96dd1b66daa912cf915056e82be4ac1e72a68
Mesfin <mesbekmek@gmail.com>
2015-11-23 00:07:09 -0500

    storyboard

--- ShoutOut/Shoutout/Shoutout/AppDelegate.m
+++ ShoutOut/Shoutout/Shoutout/AppDelegate.m
@@ -45,11 +45,11 @@ NSString * const parseClientKey = @"SIHgxMqG6dEFfIiEcJOied8zI1WEn2GuCLarvP1l";
     [SORequest registerSubclass];
     [SOContacts registerSubclass];
     
-
-    self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:[[SOLoginViewController alloc] init]];
-    self.window.backgroundColor = [UIColor whiteColor];
-    [self.window makeKeyAndVisible];
-    
+//
+//  self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:[[SOLoginViewController alloc] init]];
+//    self.window.backgroundColor = [UIColor whiteColor];
+//    [self.window makeKeyAndVisible];
+//    
     
     
     //Push notifications
