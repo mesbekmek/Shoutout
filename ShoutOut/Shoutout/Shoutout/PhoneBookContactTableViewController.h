7d4e82320ed3e000890925965285244d9802789a
Jason Wang <qwang216@gmail.com>
2015-11-28 11:58:48 -0500

    address book wrapper

---
+++ ShoutOut/Shoutout/Shoutout/PhoneBookContactTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  PhoneBookContactTableViewController.h
+//  Shoutout
+//
+//  Created by Jason Wang on 11/28/15.
+//  Copyright © 2015 Mesfin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PhoneBookContactTableViewController : UITableViewController
+
+@end
