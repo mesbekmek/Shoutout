26ede25956f6d27852be4d1b0120d6b90cd9c13b
Varindra <varindrahart@gmail.com>
2015-11-29 21:20:07 -0500

    email sending works

---
+++ ShoutOut/Shoutout/Shoutout/SOShareViewController.m
@@ -0,0 +1,80 @@
+//
+//  SOShareViewController.m
+//  Shoutout
+//
+//  Created by Varindra Hart on 11/29/15.
+//  Copyright © 2015 Mesfin. All rights reserved.
+//
+
+#import "SOShareViewController.h"
+#import <MessageUI/MessageUI.h>
+
+@interface SOShareViewController () <MFMailComposeViewControllerDelegate>
+
+@end
+
+@implementation SOShareViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)backButtonTapped:(UIButton *)sender{
+    
+    [self dismissViewControllerAnimated:YES completion:nil];
+    
+}
+
+- (IBAction)sendAsEmailButtonTapped:(UIButton *)sender{
+    
+    // Email Subject
+    NSString *emailTitle = @"Test Email";
+    // Email Content
+    NSString *messageBody = [NSString stringWithFormat: @"iOS programming is so fun! Message from Shoutout: %@", self.shareUrl];
+    // To address
+    NSArray *toRecipents = [NSArray arrayWithObject:@"varindrahart@gmail.com"];
+    
+    MFMailComposeViewController *mc = [[MFMailComposeViewController alloc] init];
+    mc.mailComposeDelegate = self;
+    [mc setSubject:emailTitle];
+    [mc setMessageBody:messageBody isHTML:YES];
+    [mc setToRecipients:toRecipents];
+    
+    // Present mail view controller on screen
+    [self presentViewController:mc animated:YES completion:NULL];
+    
+}
+
+- (void) mailComposeController:(MFMailComposeViewController *)controller didFinishWithResult:(MFMailComposeResult)result error:(NSError *)error
+{
+    switch (result)
+    {
+        case MFMailComposeResultCancelled:
+            NSLog(@"Mail cancelled");
+            break;
+        case MFMailComposeResultSaved:
+            NSLog(@"Mail saved");
+            break;
+        case MFMailComposeResultSent:
+            NSLog(@"Mail sent");
+            break;
+        case MFMailComposeResultFailed:
+            NSLog(@"Mail sent failure: %@", [error localizedDescription]);
+            break;
+        default:
+            break;
+    }
+    
+    // Close the Mail Interface
+    [self dismissViewControllerAnimated:YES completion:NULL];
+}
+
+
+
+@end
